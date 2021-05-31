3a4d8c386bb754391e8f72646fc0574d65daed15
YoussraRahmouni <youssrarahmouni41@gmail.com>
2021-05-31 22:48:01 +0000

    Show list Students

---
+++ src/Form/StudentType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Student;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class StudentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('student_name')
+            ->add('student_domain')
+            ->add('student_linkedin')
+            ->add('student_cv')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Student::class,
+        ]);
+    }
+}
